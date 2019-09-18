9e41ea39ca233de5e5af9eced331f2d75fe1d8c9
mehdihasan <email2mehdihasan@gmail.com>
2019-09-18 14:59:45 +0200

    using model to show form data! Performing reading request data and processing at controller.

--- spring-mvc-demo/WebContent/WEB-INF/view/hellowworld-form.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/hellowworld-form.jsp
@@ -5,6 +5,7 @@
 
 	<h2>Spring MVC Demo - Input Form</h2>
 
+	<!-- <form action="view-form-end-result" method="get"> -->
 	<form action="process-form" method="get">
 	
 		<input type="text" name="studentName" placeholder="What's your name?" />
--- spring-mvc-demo/src/com/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,8 +14,22 @@ public class HelloWorldController {
 		return "hellowworld-form";
 	}
 	
-	@RequestMapping("process-form")
-	public String processForm() {
+	@RequestMapping("/view-form-end-result")
+	public String viewFormResult() {
 		return "helloworld";
+	}
+	
+	@RequestMapping("/process-form")
+	public String processForm(HttpServletRequest req, Model model) {
+		
+		String theName = req.getParameter("studentName");
+		
+		theName = theName.toUpperCase();
+		
+		String result = "YO! " + theName;
+		
+		model.addAttribute("message", result);
+		
+		return "helloworld-model";
 	}
 }
