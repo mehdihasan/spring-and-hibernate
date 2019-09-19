1bf6ab195849a4df5b084be4920285b1e50fdefd
mehdihasan <email2mehdihasan@gmail.com>
2019-09-19 15:53:56 +0200

    fixed the validation issue of whitespace using @InitBinder

--- spring-mvc-demo/src/com/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/springdemo/mvc/CustomerController.java
@@ -2,9 +2,12 @@ package com.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,6 +22,15 @@ import com.springdemo.mvc.model.Customer;
 @Controller
 public class CustomerController {
 
+	// add an initbinder ... to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issue for our validation
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	@RequestMapping("/show-form")
 	public String showForm(Model model) {
 		model.addAttribute("customer", new Customer());
