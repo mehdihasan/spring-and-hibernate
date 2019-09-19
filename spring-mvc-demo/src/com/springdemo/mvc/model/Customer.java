fdd93b956bffd8720841f2440a35322b0e079cc5
mehdihasan <email2mehdihasan@gmail.com>
2019-09-19 16:34:14 +0200

    making int field required - by making it Integer

--- spring-mvc-demo/src/com/springdemo/mvc/model/Customer.java
+++ spring-mvc-demo/src/com/springdemo/mvc/model/Customer.java
@@ -16,9 +16,10 @@ public class Customer {
 	private String lastName;
 	
 	
+	@NotNull(message="is required")
 	@Min(value = 0, message = "must be grater than or equal to zero")
 	@Max(value = 10, message = "must be less than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
 	
 	
 	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
@@ -42,11 +43,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
