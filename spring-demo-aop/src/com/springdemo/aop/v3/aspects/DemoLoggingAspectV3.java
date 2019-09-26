bc492b4df5d3466790a748b2e5d3a82d910ccde9
mehdihasan <email2mehdihasan@gmail.com>
2019-09-26 15:26:55 +0200

    AOP: AOP joinpoints - display method arguments

--- spring-demo-aop/src/com/springdemo/aop/v3/aspects/DemoLoggingAspectV3.java
+++ spring-demo-aop/src/com/springdemo/aop/v3/aspects/DemoLoggingAspectV3.java
@@ -1,10 +1,13 @@
 package com.springdemo.aop.v3.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.springdemo.aop.v3.Account;
 
 /**
  * 
@@ -18,7 +21,26 @@ import org.springframework.stereotype.Component;
 public class DemoLoggingAspectV3 {
 
 	@Before("com.springdemo.aop.v3.aspects.AopExpressions.forV3DaoPackage()")
-	public void beforeAddAccountAdviceV3() {
+	public void beforeAddAccountAdviceV3(JoinPoint joinPoint) {
+		
 		System.out.println("\n==========>>> Executing @Before advice on addAccount() V3");
+		
+		// display method signature
+		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+		
+		System.out.println("Method: " + signature);
+		
+		// display method arguments
+		Object[] args = joinPoint.getArgs();
+		for (Object tempArg: args) {
+			System.out.println(tempArg);
+			
+			if (tempArg instanceof Account) {
+				Account account = (Account) tempArg;
+				
+				System.out.println("account name: " + account.getName());
+				System.out.println("account level: " + account.getLevel());
+			}
+		}
 	}
 }
