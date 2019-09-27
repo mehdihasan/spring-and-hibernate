a6cf5fc9d3e7ce36b2be2d80f378ff8214bf4d64
mehdihasan <email2mehdihasan@gmail.com>
2019-09-27 11:29:28 +0200

    AOP: basic after returning advice implemented

---
+++ spring-demo-aop/src/com/springdemo/aop/after_returning/v1/LoggingAspect.java
@@ -0,0 +1,26 @@
+package com.springdemo.aop.after_returning.v1;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+	@AfterReturning(
+			pointcut = "execution(* com.springdemo.aop.after_returning.v1.AccountDAO.findAccounts(..))", 
+			returning = "result")
+	public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result) {
+		
+		// print out which method we are advising on
+		String method = joinPoint.getSignature().toShortString();
+		System.out.println("\n======>>> Executing @AfterReturning on method: " + method);
+		
+		// print out the results of the method call
+		System.out.println("\n======>>> result is: " + result);
+	}
+}
